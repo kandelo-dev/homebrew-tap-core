4a6833d2224a38c6b79e32cbbd0467e6915ca3cf
Brandon Payton <brandon@happycode.net>
2026-07-19 12:50:41 -0400

    zlib, bzip2: select new immutable bottle identities

--- Formula/bzip2.rb
+++ Formula/bzip2.rb
@@ -8,7 +8,7 @@ class Bzip2 < Formula
   url "https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz"
   sha256 "ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269"
   license "bzip2-1.0.6"
-  revision 1
+  revision 2
 
   depends_on "binaryen" => :build
   depends_on "wabt" => :build
