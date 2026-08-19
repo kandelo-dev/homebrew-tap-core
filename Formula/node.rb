34eb8d3814c82b7ebe5cc6804fd890713b2e5626
Brandon Payton <brandon@happycode.net>
2026-08-19 02:09:53 -0700

    [ABI] Declare Node native recipe tools

--- Formula/node.rb
+++ Formula/node.rb
@@ -15,9 +15,11 @@ class Node < Formula
 
   depends_on KandeloFormulaSupport::BinaryenRequirement => :build
   depends_on KandeloFormulaSupport::WabtRequirement => [:build, :test]
+  depends_on "cbindgen" => :build
   depends_on "kandelo-dev/tap-core/libcxx"
   depends_on "kandelo-dev/tap-core/openssl"
   depends_on "kandelo-dev/tap-core/zlib"
+  depends_on "rust" => :build
 
   skip_clean "bin/node"
 
